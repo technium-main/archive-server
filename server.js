ef4a8e51533ffb9477fa7494d012653789a85339
yuliya.prokofieva <yuliya.prokofieva@ladadigit.io>
2025-04-05 11:38:59 +0500

    Опять ебучие CORS

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://ivashev-education.ru',
+  origin: '*',
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type'],
 }));
