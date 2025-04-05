40aa0a4be2c661dc2bfbc5b68682be2fe48cc78b
yuliya.prokofieva <yuliya.prokofieva@ladadigit.io>
2025-04-05 11:10:03 +0500

    Добавила заголовок, чтобы архивы нормально скачивались

--- modules/download.js
+++ modules/download.js
@@ -4,7 +4,16 @@ const { promisify } = require('util');
 const streamPipeline = promisify(require('stream').pipeline);
 
 async function downloadArchive(url, outputPath) {
-  const response = await axios({ url, method: 'GET', responseType: 'stream' });
+  const response = await axios({
+    url,
+    method: 'GET',
+    responseType: 'stream',
+    headers: {
+      'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    }
+  });
   await streamPipeline(response.data, fs.createWriteStream(outputPath));
 }
 
